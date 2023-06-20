41eec6d042b6641d3c8ac00939c5746a6ef7b816
Danila <Bbunok991@yandex.ru>
2023-06-20 17:44:01 +0300

    Completed the task_10

---
+++ Task_10/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+using System;
+int Input(string message)
+{
+    Console.Write(message);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+int number = Input("Enter a three-digit number: ");
+int result = 0;
+if (number < 100 || number >= 1000) System.Console.WriteLine("You did not enter a three-digit number! Please repeat the input.");
+else
+{
+    result = (number % 100) /10;
+    Console.WriteLine($"The second digit of the number {number}: {result}");
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task_15/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет*/
+
+using System;
+Console.Write("Enter a digit: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number == 6 && number == 7)
+{
+    Console.WriteLine("This day is a day off.");
+}
+else Console.WriteLine("This day is a weekday.");
+
+
+
+
+
+
